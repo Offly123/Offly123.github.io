ea44c98833d9e3eacacb6fde84fe9e64e38d6340
Offly123 <xxxmexanik@mail.ru>
2024-10-08 17:51:52 +0300

    Add files via upload

---
+++ 5/script.js
@@ -0,0 +1,22 @@
+﻿function calculate() {
+    let r = document.getElementById("result");
+    let number = document.getElementsByName("number-of-goods");
+    let cost = document.getElementById("products");
+    if (Number(r.innerHTML) + Number(number[0].value) * Number(cost.value) > 2000) {
+        alert("У студентов нет столько денег!");
+    }
+    else {
+        if (Number.isInteger(Number(number[0].value))) {
+            r.innerHTML = Number(r.innerHTML) + Number(number[0].value) * Number(cost.value);
+        }
+        else {
+            alert("Недопустимое количество товаров!");
+        }
+    }
+    return false;
+}
+
+function erase() {
+    let r = document.getElementById("result");
+    r.innerHTML = "0";
+}
